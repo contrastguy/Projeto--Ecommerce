7e7db6bc087d9dec7db9006c190e4c096ded6ba5
Orozimbo1 <matheusorozimbo5@gmail.com>
2022-04-23 23:57:39 -0300

    js

--- Projeto-Ecommerce/JS/Login.js
+++ Projeto-Ecommerce/JS/Login.js
@@ -1,1 +1,28 @@
 console.log("Estou funcionando!");
+
+// var a = document.querySelector('a')
+// a.addEventListener('mouseenter', entrar)
+// a.addEventListener('mouseout', sair)
+
+// function entrar(){
+//      a.style.color = 'yellow'
+// }
+
+// function sair(){
+//      a.style.color = 'green'
+// }
+
+function validar(){
+
+    let email = document.getElementById('email')
+    let pass = document.getElementById('pass')
+
+    if (email.value.length == 0 || pass.value.length == 0){
+        alert('[Error] E-mail ou senha inválidos')
+    }else if(email.value === "projetotera@tera.com" && pass.value === "12345"){
+        window.location.href = "./PerfilUsuário.html"
+    }else{
+        alert('[Error] E-mail ou senha inválidos')
+    }
+}
+
--- Projeto-Ecommerce/JS/PerfilUsuário.js
+++ Projeto-Ecommerce/JS/PerfilUsuário.js
@@ -1,1 +1,19 @@
 console.log("Estou funcionando!");
+
+let mudanca = document.getElementsByTagName('li')
+
+function entrar(){
+    console.log(mudanca)
+    mudanca
+}
+
+function sair(){
+    console.log(mudanca)
+}
+
+
+
+
+
+
+
