584d130e36da9b598d3ff12a06867fcea2443776
Lucas Freitas <l.freittas@outlook.com>
2022-04-25 23:07:44 -0300

    junção das pag

--- Projeto-Ecommerce/JS/PerfilUsuário.js
+++ Projeto-Ecommerce/JS/PerfilUsuário.js
@@ -1,1 +1,11 @@
-console.log("Estou funcionando!");
+let listas = document.getElementsByClassName('lista')
+
+for (let i = 0; i < listas.length; i++) {
+    listas[i].addEventListener("mouseover", function () {
+        this.classList.add("destaque")
+    })
+
+listas[i].addEventListener("mouseleave", function () {
+    this.classList.remove("destaque")
+})
+}
