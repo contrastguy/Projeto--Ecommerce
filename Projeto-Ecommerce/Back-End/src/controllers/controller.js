b1af7f5ff5eccc6b5cfc24a64993326ae58c205a
Matheus Rodrigues <62529344+contrastguy@users.noreply.github.com>
2022-06-26 21:49:13 -0300

    Update controller.js

--- Projeto-Ecommerce/Back-End/src/controllers/controller.js
+++ Projeto-Ecommerce/Back-End/src/controllers/controller.js
@@ -3,43 +3,90 @@ import User from "../models/user.js"
 import mongoose from "mongoose";
 
 
-class Controllerfunctions{
-    static getAll = async(req,res) => {
-        const { name,email,password } = req.body;
+class Controllerfunctions {
+  static getAll = async (req, res) => {
+    try {
 
-    const possibleUser = await User.find({ name,email,password })
-    //a condição do if é True , já esta como padrão
-    if (possibleUser) {
-        console.log({possibleUser})
-      return res.status(400).send({ error: "Usuário já cadastrado"})
+      const { name, email, password } = req.body
+
+      const possibleUser = await User.find({ name, email, password }).populate("User")
+      //a condição do if é True , já esta como padrão
+      if (possibleUser) {
+
+        return res.status(200).send([{ error: "Usuário já cadastrado" }, { possibleUser }])
+      }
+      //else
+      const newuser = await User.create(req.body)
+      User.password = undefined;
+      return res.status(200).send({ newuser })
+
+    } catch (error) {
+
+      res.status(400).send({ error: "Erro com a lógica da função" })
     }
-    //else
-    const newuser = await User.create(req.body)
-    user.password = undefined;
-    return res.status(200).send({ newuser })
 
-    // const usuario = new User ({
-    //     name:"Matheus",
-    //     email:"matheus@email.com"
-    // })
 
-    // if(usuario){
-    //     const findUsuario = await usuario.~nome da função~()
-    //     return res.status(200).send({findUsuario})
-    // }else{
-    //     const newuser = await User.create(req.body)
-    //     user.password = undefined;
-    //     return res.status(200).send({ newuser })
-    // }
+  }
 
-  },
-
+  static getById = async(req,res) => {
+    try {
+      
+      const reqid = req.params.id
     
-
-
-   
-               
+      const find = await User.findById(reqid)
+      res.send(find)
+    } catch (error) {
+      console.log(error)
+      res.status(400).send({ error: "Erro com a lógica da função" })
     }
+  }
+
+
+
+
+  static createUser = (req, res) => {
+    try {
+      let newuser =new User({
+        name: User.name,
+        email: User.email,
+        password: User.password
+      })
+
+
+      res.status(201).send([{ Message: "Usuário criado" },{ newuser }])
+    } catch (error) {
+      console.log(error)
+      res.status(400).send({ error: "Erro com a lógica da função" })
+    }
+  }
+
+
+
+  static updateUser = async (req, res) => {
+    try {
+      const id = (req.params.id)
+      const user = (req.body)
+
+    } catch (error) {
+      console.log(error)
+      res.status(400).send({ error: "Erro com a lógica da função" })
+    }
+  }
+
+
+  static deleteUser = async (req, res) => {
+    try {
+        const Remove = await User.findByIdAndDelete(req.params.id)
+        res.send({Remove})
+    } catch (error) {
+      console.log(error)
+      res.status(400).send({ error: "Erro com a lógica da função" })
+    }
+  }
+
+
+
+}
 
 
 export default Controllerfunctions;
