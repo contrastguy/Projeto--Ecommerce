9681e03364ab3fc63067dd03c0dbbc88d8d92ddf
contrastguy <kgmatheus11@gmail.com>
2022-06-30 10:22:04 -0300

    Deleted files

--- Projeto-Ecommerce/Back-End/src/config/ProductsUsers.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-
-mongoose.connect("mongodb://localhost:27017/Products&Users")
-
-const ProductsUsers = mongoose.connection
-
-export default ProductsUsers
-
--- README.md
+++
@@ -1,3 +0,0 @@
-# Projeto--Ecommerce
-Ecommerce B2C2C de uma Loja de Roupas 
-
